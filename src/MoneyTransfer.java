5f31ffa33d94015d75288509474789f08af89d45
FilStas <filstas23@gmail.com>
2020-04-29 20:16:02 +0400

    add file project

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int deposit = 2_000_000_000;
+        int transfer = 500_000_000;
+        int result = deposit + transfer;
+        System.out.println(result);
+    }
+}
